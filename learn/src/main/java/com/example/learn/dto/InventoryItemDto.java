42bbcdfd26803f57996a995dbb69eed48248eb95
darklight-amit <amit.rajawat12@gmail.com>
2025-12-23 19:41:53 +0000

    draft 2

---
+++ learn/src/main/java/com/example/learn/dto/InventoryItemDto.java
@@ -0,0 +1,15 @@
+package com.examle.learn.dto;
+
+public class InventoryItemDto{
+
+    
+
+    private int barcode;
+    private String item;
+    private String category;
+    private double price;
+    private int discount;
+    private int available;
+
+    // getter and setter
+}
---
+++ learn/src/main/java/com/example/learn/dto/InverntoryResponseDto.java
@@ -0,0 +1,12 @@
+package com.examle.learn.dto;
+
+public class InventoryResponseDto{
+
+    private int page;
+    private int per_page;
+    private int total;
+    private int total_pages;
+    private List<InventoryItemDto> data;
+
+    // getter and setter
+}
