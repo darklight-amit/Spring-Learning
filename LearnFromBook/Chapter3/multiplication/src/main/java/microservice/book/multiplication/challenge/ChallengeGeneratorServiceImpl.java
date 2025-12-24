bc4915d120355b53382f4466051a7728f2ed4b01
darklight-amit <amit.rajawat12@gmail.com>
2025-12-24 10:48:07 +0000

    successfully pass first test case

--- LearnFromBook/Chapter3/multiplication/src/main/java/microservice/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
+++ LearnFromBook/Chapter3/multiplication/src/main/java/microservice/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
@@ -7,18 +7,22 @@ import java.util.Random;
 @Service
 public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService{
 
+    private final static int MINIMUM_FACTOR = 11;
+    private final static int MAXIMUM_FACTOR = 100;
     private final Random random;
 
-    ChallengeGeneratorServiceImpl(){
-        this.random  = new Random();
+    ChallengeGeneratorServiceImpl() {
+        this.random = new Random();
     }
-
-    protected ChallengeGeneratorServiceImpl(final Random random){
+    protected ChallengeGeneratorServiceImpl(final Random random) {
         this.random = random;
     }
-
+  
+    private int next() {
+        return random.nextInt(MAXIMUM_FACTOR - MINIMUM_FACTOR) + MINIMUM_FACTOR;
+    }
     @Override
-    public Challenge randomChallenge(){
-        return null;
+    public Challenge randomChallenge() {
+        return new Challenge(next(), next());
     }
 }
