c524fa8e96ef9775c2794f5e51aab26310368901
darklight-amit <amit.rajawat12@gmail.com>
2025-12-25 04:13:06 +0000

    added challange service

---
+++ LearnFromBook/Chapter3/multiplication/src/test/java/microservice/book/multiplication/challenge/ChallengeServiceTest.java
@@ -0,0 +1,13 @@
+package microservice.book.multiplication.challenge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class ChallangeServiceTest{
+
+
+
+
+}
