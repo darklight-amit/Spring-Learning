0e73554a0825b72ae1ec900e32dbc780f8a87d34
amit rajawat <amit.rajawat@vodafone.com>
2025-12-25 10:13:21 +0530

    attempt test draft

--- LearnFromBook/Chapter3/multiplication/src/test/java/microservice/book/multiplication/challenge/ChallengeServiceTest.java
+++ LearnFromBook/Chapter3/multiplication/src/test/java/microservice/book/multiplication/challenge/ChallengeServiceTest.java
@@ -1,5 +1,6 @@
 package microservice.book.multiplication.challenge;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,22 @@ public class ChallangeServiceTest{
 
     private ChallengeService challengeService;
 
+    @BeforeEach
+    public void setup(){
+        challengeService = new ChallengeServiceImpl();
+    }
+
+    @Test
+    public void checkCorrectAttemptTest(){
+        //given
+        ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(20,30,"Amit",6000);
+
+        //when
+        ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+
+        //then
+
+        then(resultAttempt.isCorrect()).isTrue();
+    }
 
 }
